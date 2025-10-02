84abe8b2064248a4043c77fcf82c749a6a7c5d6e
Lucassamuel97 <lukassamuka88@gmail.com>
2025-10-02 09:39:16 -0300

    :test_tube: test: Adiciona testes para atualização de itens na API

--- infrastructure/src/test/java/com/starter/crudexample/infrastructure/api/ItemAPITest.java
+++ infrastructure/src/test/java/com/starter/crudexample/infrastructure/api/ItemAPITest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -33,12 +34,15 @@ import com.starter.crudexample.application.item.create.CreateItemOutput;
 import com.starter.crudexample.application.item.create.DefaultCreateItemUseCase;
 import com.starter.crudexample.application.item.retrieve.get.DefaultGetItemByIdUseCase;
 import com.starter.crudexample.application.item.retrieve.get.ItemOutput;
+import com.starter.crudexample.application.item.update.DefaultUpdateItemUseCase;
+import com.starter.crudexample.application.item.update.UpdateItemOutput;
 import com.starter.crudexample.domain.exceptions.NotFoundException;
 import com.starter.crudexample.domain.exceptions.NotificationException;
 import com.starter.crudexample.domain.item.Item;
 import com.starter.crudexample.domain.item.ItemID;
 import com.starter.crudexample.domain.validation.Error;
 import com.starter.crudexample.infrastructure.item.models.CreateItemRequest;
+import com.starter.crudexample.infrastructure.item.models.UpdateItemRequest;
 
 @ControllerTest(controllers = ItemAPI.class)
 public class ItemAPITest {
@@ -54,6 +58,9 @@ public class ItemAPITest {
 
         @MockitoBean
         private DefaultGetItemByIdUseCase getItemByIdUseCase;
+
+        @MockitoBean
+        private DefaultUpdateItemUseCase updateItemUseCase;
 
         @Test
         public void givenAValidCommand_whenCallsCreateItem_thenShouldReturnItemId() throws Exception {
@@ -169,5 +176,75 @@ public class ItemAPITest {
                 response.andExpect(status().isNotFound());
 
                 verify(getItemByIdUseCase).execute(eq(expectedId.getValue()));
+        }
+
+        @Test
+        public void givenAValidCommand_whenCallsUpdateItem_shouldReturnItsIdentifier() throws Exception {
+                // given
+                final var expectedName = "Item name";
+                final var expectedDescription = "Item description";
+                final var expectedPrice = 10.0;
+
+                final var aItem = Item.newItem(expectedName, expectedDescription, expectedPrice);
+                final var expectedId = aItem.getId();
+
+                final var aCommand = new UpdateItemRequest(expectedName, expectedDescription, expectedPrice);
+
+                when(updateItemUseCase.execute(any()))
+                                .thenReturn(UpdateItemOutput.from(expectedId));
+
+                // when
+                final var aRequest = put("/items/{id}", expectedId.getValue())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(aCommand));
+
+                final var response = this.mvc.perform(aRequest)
+                                .andDo(print());
+
+                // then
+                response.andExpect(status().isOk())
+                                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                                .andExpect(jsonPath("$.id", equalTo(expectedId.getValue())));
+
+                verify(updateItemUseCase)
+                                .execute(argThat(actualCmd -> Objects.equals(expectedId.getValue(), actualCmd.id())
+                                                && Objects.equals(expectedName, actualCmd.name())
+                                                && Objects.equals(expectedDescription, actualCmd.description())
+                                                && Objects.equals(expectedPrice, actualCmd.price())));
+        }
+
+        @Test
+        public void givenAnInvalidName_whenCallsUpdateItem_shouldReturnNotification() throws Exception {
+                // given
+                final var expectedId = ItemID.from("123");
+                final String expectedName = null;
+                final var expectedDescription = "Item description";
+                final var expectedPrice = 10.0;
+                final var expectedMessage = "'name' should not be null";
+
+                final var aCommand = new UpdateItemRequest(expectedName, expectedDescription, expectedPrice);
+
+                when(updateItemUseCase.execute(any()))
+                                .thenThrow(NotificationException.with(new Error(expectedMessage)));
+
+                // when
+                final var aRequest = put("/items/{id}", expectedId.getValue())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(aCommand));
+
+                final var response = this.mvc.perform(aRequest)
+                                .andDo(print());
+
+                // then
+                response.andExpect(status().isUnprocessableEntity())
+                                .andExpect(header().string("Location", nullValue()))
+                                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                                .andExpect(jsonPath("$.errors", hasSize(1)))
+                                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+                verify(updateItemUseCase)
+                                .execute(argThat(actualCmd -> Objects.equals(expectedId.getValue(), actualCmd.id())
+                                                && Objects.equals(expectedName, actualCmd.name())
+                                                && Objects.equals(expectedDescription, actualCmd.description())
+                                                && Objects.equals(expectedPrice, actualCmd.price())));
         }
 }
